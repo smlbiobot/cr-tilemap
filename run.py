df8ce1532e442f2028eb4706e045c66c94ce802a
SML <smlbiobot@gmail.com>
2017-10-12 18:25:18 +0800

    tilemap completed

--- run.py
+++ run.py
@@ -22,6 +22,17 @@ def camelcase_split(s):
 
 class App:
 
+    colors = {
+        '-1': (0.9, 0.9, 0.9),
+        '0': (0.9, 0.9, 0.9),
+        '1': (0.5, 0.9, 0.9),
+        '2': (0.9, 0.9, 0.5),
+        '16': (0.7, 0.5, 0.5),
+        '17': (0.5, 0.7, 0.5),
+        '18': (0.5, 0.5, 0.7),
+        '32': (0.7, 0.7, 0.7)
+    }
+
     def __init__(self, config_path=None):
         with open(config_path) as f:
             self.config = Box(yaml.load(f))
@@ -32,14 +43,16 @@ class App:
         for t in self.config.tilemaps:
             self.make_tilemap(t)
 
-    def draw_tile(self, c, x, y):
+    def draw_tile(self, c, x, y, value):
         w = 1
         h = 1
-        s = 0.4 * cm
+        s = 0.3 * cm
+        x0 = 1 * cm
+        y0 = 1 * cm
         c.setLineWidth(0.3)
         c.setStrokeColorRGB(0.2, 0.2, 0.2)
-        c.setFillColorRGB(0.9, 0.9, 0.9)
-        c.rect(x * s, y * s, w * s, h * s, fill=1)
+        c.setFillColorRGB(*self.colors[value])
+        c.rect(x0 + x * s, y0 + y * s, w * s, h * s, fill=1)
 
     def make_tilemap(self, config):
         """Generate tilemap from csv to pdf."""
@@ -49,14 +62,25 @@ class App:
         with open(config.csv) as f:
             reader = csv.reader(f)
             y = 0
+            start = -10000
             for row in reader:
                 x = 0
-                for col in row:
-                    print(col)
-                    self.draw_tile(c, x, y)
-                    x += 1
+                if row[0] == 'Map':
+                    start = -2
+
+                if row[0] == 'Layout':
+                    start = -10000
+
+                if start > 0:
+                    for value in row:
+                        if x > 0:
+                            if value == '':
+                                value = '-1'
+                            self.draw_tile(c, x, y, value)
+                        x += 1
                 y += 1
-        self.draw_tile(c, 1, 1)
+                start += 1
+
         c.showPage()
         c.save()
 
